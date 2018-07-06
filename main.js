672a386e557d6e0385c62a8e9ff7271e1c7dcb9b
jhassler1031 <jhassler1031@gmail.com>
2018-07-06 12:30:48 -0400

    adding function to display all tags for all items in the sidebar, working but way too much data

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 //Make the ajax call to get the data
 let baseURL = "https://api.etsy.com/v2/listings/active.js"
 //API key being pulled in from key.js
+let keyword = "bearded%20dragon";
 
 $.ajax({
-    url: `${baseURL}?api_key=${apiKey}&keywords=telescopes&includes=Images,Shop&limit=25`,
+    url: `${baseURL}?api_key=${apiKey}&keywords=${keyword}&includes=Images,Shop&limit=25`,
     type: "GET",
     dataType: "jsonp",
     success: getResults
@@ -14,12 +15,25 @@ $.ajax({
 function getResults(res) {
   console.log(res);
 
-  // Start handlebars ============================================================
+  // Handlebar template for tiles ==============================================
   var source = document.getElementById("tile-template").innerHTML;
   var template = Handlebars.compile(source);
-
   var context = res;
   var html = template(context);
-
   $(".tiles-row").html(html);
+
+  // var sidebarSource = document.getElementById("sidebar-template").innerHTML;
+  // var sidebarTemplate = Handlebars.compile(sidebarSource);
+  // var sidebarContext = res.
+  let $sideBar = document.querySelector(".side-bar");
+  let results = res.results
+  for (let y = 0; y < results.length; y++) {
+    let tags = results[y].tags;
+    console.log(tags);
+    for (let x = 0; x < tags.length; x++) {
+      console.log(tags[x]);
+      // $sideBar.append(`<a>${tags[x]})</a>`);
+    }
+  }
+
 }
